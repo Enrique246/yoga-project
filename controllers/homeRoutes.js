d59255da41549d59e4870222d60643de4f2191f2
Henri <ecch9210@gmail.com>
2021-06-24 20:11:09 -0500

    Home, Profile and login working

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -12,64 +12,95 @@ router.get('/', async (req, res) => {
     res.status(500).json(err);
   }
 });
-router.get("/book", async (req, res) => {
-  try {
-    const schedule = [
-      {
-        day: "Monday",
-        teacher: "Andres",
-        availableSpots: ["spot one", "spot two"],
-      },
-      {
-        day: "Tuesday",
-        teacher: "Andres",
-        availableSpots: ["spot one", "spot two"],
-      },
-      {
-        day: "Wednesday",
-        teacher: "Andres",
-        availableSpots: ["spot one", "spot two"],
-      },
-    ];
 
-    res.render("book", {schedule,
-      loggedIn: req.session.logged_in,
-      book: true
-    })
-  } catch (err) {
-    res.status(500).json(err)
-  }
-});
-// router.get('/book ', async (req, res) => {
-//    try {
-//     const classesData = await Classes.findByPk(req.params.id, {
-//       include: [
-//         {
-//           model: Class,
-//         },
-//       ],
-//     });
-//     const classes = classesData.get({ plain: true });
-//     res.render('book', {
-//       ...classes,
-//       logged_in: req.session.logged_in,
+// router.get("/book", async (req, res) => {
+//   try {
+//     const schedule = [
+//       {
+//         id:1,
+//         class_name: "Hatha Yoga",
+//         date: "2021-06-24",
+//         time: "12:00:00",
+//         coach: "Andres",
+//         available:5,
+   
+       
+//       },
+//       {
+//         id:2,
+//         class_name: "Ashtanga Yoga",
+//         date: "2021-06-25",
+//         time: "11:00:00",
+//         coach: "Adolfo",
+//         available:3,
+       
+//       },
+   
+//       {
+//         id:3,
+//         class_name: "Kundalini Yoga",
+//         date: "2021-06-26",
+//         time: "10:00:00",
+//         coach: "Miranda",
+//         available:4,
+       
+//       },
+//       {
+//         id:4,
+//         class_name: "KBikram Yoga",
+//         date: "2021-06-29",
+//         time: "15:00:00",
+//         coach: "Enrique",
+//         available:6,
+       
+//       },
+//       {
+//         id:5,
+//         class_name: "Aero Yoga",
+//         date: "2021-06-30",
+//         time: "09:00:00",
+//         coach: "Andres",
+//         available:2,
+       
+//       }
+//     ];
+//     res.render("book", {schedule,
+//       loggedIn: req.session.logged_in,
 //       book: true
-//     });
+//     })
 //   } catch (err) {
-//     res.status(500).json(err);
+//     res.status(500).json(err)
 //   }
 // });
+router.get('/book', withAuth, async (req, res) => {
+  try {
+    // Find the logged in user based on the session ID
+    const classesData = await Classes.findAll( {
+      include: Classes,
+    });
+    const classes = classesData.get({ plain: true });
+    console.log(classes)
+    res.render('book', {
+      ...classes,
+      logged_in: req.session.logged_in,
+      book: true
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
 // Use withAuth middleware to prevent access to route
 router.get('/profile', withAuth, async (req, res) => {
   try {
     // Find the logged in user based on the session ID
-    // const userData = await User.findByPk(req.session.user_id, {
-    //   attributes: { exclude: ['password'] },
-    //   include: [{ model: Reservations }],
-    // });
-    // const user = userData.get({ plain: true });
+    const userData = await User.findByPk(req.session.user_id, {
+      include: Classes,
+    });
+    const user = userData.get({ plain: true });
+    console.log(user)
     res.render('profile', {
-      // ...user,
+      ...user,
       logged_in: true,
       profile: true
     });
--- models/index.js
+++ models/index.js
@@ -1,5 +1,13 @@
 const Classes = require('./Classes');
 const Reservation = require('./Reservations');
-const User = require('./Users')
+const User = require('./Users');
+const sequelize = require('sequelize')
 
+User.belongsToMany(Classes,{
+    through:"reservations",
+})
+
+Classes.belongsToMany(User,{
+    through:"reservations",
+})
 module.exports = { Classes, Reservation, User};
